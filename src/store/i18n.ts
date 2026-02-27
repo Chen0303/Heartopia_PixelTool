1b9657b5bd091a3bcb2b875b05229053d688e9da
Fangling-Chen <50167522+Chen0303@users.noreply.github.com>
2026-02-27 20:05:06 +0800

    Delete src directory

--- src/store/i18n.ts
+++
@@ -1,8 +0,0 @@
-import { ref } from "vue";
-
-export const currentLang = ref<"zh" | "en">("zh");
-export const translations = ref<Record<string, any>>({});
-
-export function t(key: string) {
-  return translations.value[key]?.[currentLang.value] || key;
-}
